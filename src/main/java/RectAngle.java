48bb7eb4b5187776ef39f24a17b39df666db8879
Mahdihma <mahdi.haji@yahoo.com>
2022-08-05 10:41:11 +0430

    Implement First Test

---
+++ src/main/java/RectAngle.java
@@ -0,0 +1,10 @@
+public class RectAngle implements Shape{
+    private double width;
+    private double height;
+
+    public RectAngle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+}
---
+++ src/test/java/ShapeTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ShapeTest {
+    @Test
+    public void testRectangleArea() {
+        Shape rectAngle = new RectAngle(2,3);
+        double result = rectAngle.ComputeArea();
+        assertEquals(6, result, 0.05);
+        RectAngle zeroRect = new RectAngle(0, 3);
+        result = rectAngle.ComputeArea();
+        assertEquals(0, result, 0.05);
+
+    }
+}
